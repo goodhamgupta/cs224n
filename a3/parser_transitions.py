549bdba9ca52bddbba4692b8a5a4a8adc2210e63
Shubham Gupta <shubhamg2208@live.com>
2020-01-20 17:59:06 +0800

    Complete parts c and d in parser assignment

--- a3/parser_transitions.py
+++ a3/parser_transitions.py
@@ -7,6 +7,8 @@ Sahil Chopra <schopra8@stanford.edu>
 """
 
 import sys
+from copy import deepcopy
+
 
 class PartialParse(object):
     def __init__(self, sentence):
@@ -30,10 +32,11 @@ class PartialParse(object):
         ###
         ### Note: The root token should be represented with the string "ROOT"
         ###
-
+        self.stack = ["ROOT"]
+        self.buffer = deepcopy(sentence)
+        self.dependencies = []
 
         ### END YOUR CODE
-
 
     def parse_step(self, transition):
         """Performs a single parse step by applying the given transition to this partial parse
@@ -49,7 +52,18 @@ class PartialParse(object):
         ###         1. Shift
         ###         2. Left Arc
         ###         3. Right Arc
-
+        if transition == "S":
+            word = self.buffer.pop(0)
+            self.stack.append(word)
+        else:
+            w_i, w_j = self.stack[-2:]
+            self.stack = self.stack[:-2]
+            if transition == "LA":
+                self.dependencies.append((w_j, w_i))
+                self.stack.append(w_j)
+            else:
+                self.dependencies.append((w_i, w_j))
+                self.stack.append(w_i)
 
         ### END YOUR CODE
 
@@ -101,26 +115,42 @@ def minibatch_parse(sentences, model, batch_size):
     ###             to remove objects from the `unfinished_parses` list. This will free the underlying memory that
     ###             is being accessed by `partial_parses` and may cause your code to crash.
 
-
     ### END YOUR CODE
-
+    partial_parses = list(map(lambda x: PartialParse(x), sentences))
+    unfinished_parses = partial_parses[:]
+    while len(unfinished_parses) > 0:
+        minibatch = unfinished_parses[:batch_size]
+        next_steps = model.predict(minibatch)
+        for index, record in enumerate(minibatch):
+            record.parse_step(next_steps[index])
+            if not record.buffer and len(record.stack) == 1:
+                unfinished_parses.remove(record)
+    dependencies = [record.dependencies for record in partial_parses]
     return dependencies
 
 
-def test_step(name, transition, stack, buf, deps,
-              ex_stack, ex_buf, ex_deps):
+def test_step(name, transition, stack, buf, deps, ex_stack, ex_buf, ex_deps):
     """Tests that a single parse step returns the expected output"""
     pp = PartialParse([])
     pp.stack, pp.buffer, pp.dependencies = stack, buf, deps
 
     pp.parse_step(transition)
-    stack, buf, deps = (tuple(pp.stack), tuple(pp.buffer), tuple(sorted(pp.dependencies)))
-    assert stack == ex_stack, \
-        "{:} test resulted in stack {:}, expected {:}".format(name, stack, ex_stack)
-    assert buf == ex_buf, \
-        "{:} test resulted in buffer {:}, expected {:}".format(name, buf, ex_buf)
-    assert deps == ex_deps, \
-        "{:} test resulted in dependency list {:}, expected {:}".format(name, deps, ex_deps)
+    stack, buf, deps = (
+        tuple(pp.stack),
+        tuple(pp.buffer),
+        tuple(sorted(pp.dependencies)),
+    )
+    assert stack == ex_stack, "{:} test resulted in stack {:}, expected {:}".format(
+        name, stack, ex_stack
+    )
+    assert buf == ex_buf, "{:} test resulted in buffer {:}, expected {:}".format(
+        name, buf, ex_buf
+    )
+    assert (
+        deps == ex_deps
+    ), "{:} test resulted in dependency list {:}, expected {:}".format(
+        name, deps, ex_deps
+    )
     print("{:} test passed!".format(name))
 
 
@@ -128,12 +158,36 @@ def test_parse_step():
     """Simple tests for the PartialParse.parse_step function
     Warning: these are not exhaustive
     """
-    test_step("SHIFT", "S", ["ROOT", "the"], ["cat", "sat"], [],
-              ("ROOT", "the", "cat"), ("sat",), ())
-    test_step("LEFT-ARC", "LA", ["ROOT", "the", "cat"], ["sat"], [],
-              ("ROOT", "cat",), ("sat",), (("cat", "the"),))
-    test_step("RIGHT-ARC", "RA", ["ROOT", "run", "fast"], [], [],
-              ("ROOT", "run",), (), (("run", "fast"),))
+    test_step(
+        "SHIFT",
+        "S",
+        ["ROOT", "the"],
+        ["cat", "sat"],
+        [],
+        ("ROOT", "the", "cat"),
+        ("sat",),
+        (),
+    )
+    test_step(
+        "LEFT-ARC",
+        "LA",
+        ["ROOT", "the", "cat"],
+        ["sat"],
+        [],
+        ("ROOT", "cat",),
+        ("sat",),
+        (("cat", "the"),),
+    )
+    test_step(
+        "RIGHT-ARC",
+        "RA",
+        ["ROOT", "run", "fast"],
+        [],
+        [],
+        ("ROOT", "run",),
+        (),
+        (("run", "fast"),),
+    )
 
 
 def test_parse():
@@ -143,11 +197,17 @@ def test_parse():
     sentence = ["parse", "this", "sentence"]
     dependencies = PartialParse(sentence).parse(["S", "S", "S", "LA", "RA", "RA"])
     dependencies = tuple(sorted(dependencies))
-    expected = (('ROOT', 'parse'), ('parse', 'sentence'), ('sentence', 'this'))
-    assert dependencies == expected,  \
-        "parse test resulted in dependencies {:}, expected {:}".format(dependencies, expected)
-    assert tuple(sentence) == ("parse", "this", "sentence"), \
-        "parse test failed: the input sentence should not be modified"
+    expected = (("ROOT", "parse"), ("parse", "sentence"), ("sentence", "this"))
+    assert (
+        dependencies == expected
+    ), "parse test resulted in dependencies {:}, expected {:}".format(
+        dependencies, expected
+    )
+    assert tuple(sentence) == (
+        "parse",
+        "this",
+        "sentence",
+    ), "parse test failed: the input sentence should not be modified"
     print("parse test passed!")
 
 
@@ -156,46 +216,70 @@ class DummyModel(object):
     First shifts everything onto the stack and then does exclusively right arcs if the first word of
     the sentence is "right", "left" if otherwise.
     """
+
     def predict(self, partial_parses):
-        return [("RA" if pp.stack[1] is "right" else "LA") if len(pp.buffer) == 0 else "S"
-                for pp in partial_parses]
+        return [
+            ("RA" if pp.stack[1] is "right" else "LA") if len(pp.buffer) == 0 else "S"
+            for pp in partial_parses
+        ]
 
 
 def test_dependencies(name, deps, ex_deps):
     """Tests the provided dependencies match the expected dependencies"""
     deps = tuple(sorted(deps))
-    assert deps == ex_deps, \
-        "{:} test resulted in dependency list {:}, expected {:}".format(name, deps, ex_deps)
+    assert (
+        deps == ex_deps
+    ), "{:} test resulted in dependency list {:}, expected {:}".format(
+        name, deps, ex_deps
+    )
 
 
 def test_minibatch_parse():
     """Simple tests for the minibatch_parse function
     Warning: these are not exhaustive
     """
-    sentences = [["right", "arcs", "only"],
-                 ["right", "arcs", "only", "again"],
-                 ["left", "arcs", "only"],
-                 ["left", "arcs", "only", "again"]]
+    sentences = [
+        ["right", "arcs", "only"],
+        ["right", "arcs", "only", "again"],
+        ["left", "arcs", "only"],
+        ["left", "arcs", "only", "again"],
+    ]
     deps = minibatch_parse(sentences, DummyModel(), 2)
-    test_dependencies("minibatch_parse", deps[0],
-                      (('ROOT', 'right'), ('arcs', 'only'), ('right', 'arcs')))
-    test_dependencies("minibatch_parse", deps[1],
-                      (('ROOT', 'right'), ('arcs', 'only'), ('only', 'again'), ('right', 'arcs')))
-    test_dependencies("minibatch_parse", deps[2],
-                      (('only', 'ROOT'), ('only', 'arcs'), ('only', 'left')))
-    test_dependencies("minibatch_parse", deps[3],
-                      (('again', 'ROOT'), ('again', 'arcs'), ('again', 'left'), ('again', 'only')))
+    test_dependencies(
+        "minibatch_parse",
+        deps[0],
+        (("ROOT", "right"), ("arcs", "only"), ("right", "arcs")),
+    )
+    test_dependencies(
+        "minibatch_parse",
+        deps[1],
+        (("ROOT", "right"), ("arcs", "only"), ("only", "again"), ("right", "arcs")),
+    )
+    test_dependencies(
+        "minibatch_parse",
+        deps[2],
+        (("only", "ROOT"), ("only", "arcs"), ("only", "left")),
+    )
+    test_dependencies(
+        "minibatch_parse",
+        deps[3],
+        (("again", "ROOT"), ("again", "arcs"), ("again", "left"), ("again", "only")),
+    )
     print("minibatch_parse test passed!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = sys.argv
     if len(args) != 2:
-        raise Exception("You did not provide a valid keyword. Either provide 'part_c' or 'part_d', when executing this script")
+        raise Exception(
+            "You did not provide a valid keyword. Either provide 'part_c' or 'part_d', when executing this script"
+        )
     elif args[1] == "part_c":
         test_parse_step()
         test_parse()
     elif args[1] == "part_d":
         test_minibatch_parse()
     else:
-        raise Exception("You did not provide a valid keyword. Either provide 'part_c' or 'part_d', when executing this script")
+        raise Exception(
+            "You did not provide a valid keyword. Either provide 'part_c' or 'part_d', when executing this script"
+        )
